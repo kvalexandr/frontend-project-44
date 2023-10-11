fa5b983f2ec1c1637cd3a98f04971a415d86e679
Alexandr Kudryavtcev <k.v.alexandr@gmail.com>
2023-10-11 21:40:25 +0400

    add: games prime

---
+++ src/games/prime.js
@@ -0,0 +1,27 @@
+import initGame from '../index.js';
+import getRandomNumber from '../helpers.js';
+
+const startQuestion = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (n) => {
+  if (n < 2) return false;
+  for (let i = 2; i <= n / 2; i += 1) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+};
+
+const game = () => {
+  const number = getRandomNumber(1, 100);
+  const question = number;
+  const answer = isPrime(number) ? 'yes' : 'no';
+
+  return [question, answer];
+};
+
+const runGame = () => {
+  initGame(startQuestion, game);
+};
+
+export default runGame;
